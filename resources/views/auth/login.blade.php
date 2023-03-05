843128f5a4bab6545a29d60a06f8b8aee678fdc0
3ngBakr <119337108+3ngBakr@users.noreply.github.com>
2023-03-05 11:52:05 +0300

    login form
    login form modifaction

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,50 +1,79 @@
 <x-guest-layout>
-    <x-jet-authentication-card>
-        <x-slot name="logo">
-            <x-jet-authentication-card-logo />
-        </x-slot>
 
-        <x-jet-validation-errors class="mb-4" />
 
-        @if (session('status'))
-            <div class="mb-4 font-medium text-sm text-green-600">
-                {{ session('status') }}
-            </div>
-        @endif
+    <!doctype html>
+<html lang="en">
+  <head>
+  	<title>Login 07</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
-        <form method="POST" action="{{ route('login') }}">
-            @csrf
+	<link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900&display=swap" rel="stylesheet">
 
-            <div>
-                <x-jet-label for="email" value="{{ __('Email') }}" />
-                <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
-            </div>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	
+	<link rel="stylesheet" href="{{ asset('login-form-11\css\style.css') }}">
 
-            <div class="mt-4">
-                <x-jet-label for="password" value="{{ __('Password') }}" />
+	</head>
+	<body>
+	<section class="ftco-section">
+		<div class="container">
+			<div class="row justify-content-center">
+				<div class="col-md-12 col-lg-10">
+					<div class="wrap d-md-flex">
+						<div class="text-wrap p-4 p-lg-5 text-center d-flex align-items-center order-md-last">
+							<div class="text w-100">
+								<h2> مرحبا بكم في   </h2>
+								<p>إرشيف مكتب وزير الأوقاف والإرشاد</p>
+							</div>
+			      </div>
+						<div class="login-wrap p-4 p-lg-5">
+			      	<div class="d-flex">
+			      		<div class="w-100">
+			      			<h3 class="mb-4 float-right">تسجيل الدخول</h3>
+			      		</div>
+							
+			      	</div>
+							<form method="POST" action="{{ route('login') }}" class="signin-form">
+			      		<div class="form-group mb-3">
+			      			<label class="label float-right" for="name">البريد الإلكتروني</label>
+			      			<input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus>
+			      		</div>
+		            <div class="form-group mb-3 ">
+		            	<x-jet-label for="password" class="float-right" value="{{ __('Password') }}" />
                 <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required autocomplete="current-password" />
-            </div>
-
-            <div class="block mt-4">
-                <label for="remember_me" class="flex items-center">
+           </div>
+		            <div class="form-group">
+		            	<button type="submit" class="form-control btn btn-primary submit px-3">{{ __('Log in') }}</button>
+		            </div>
+		            <div class="form-group d-md-flex">
+		            	<div class="w-50 text-left">
+                            <label for="remember_me" class="flex items-center">
                                 <label for="remember_me" class="flex items-center">
 
                     <x-jet-checkbox id="remember_me" name="remember" />
                     <span class="ml-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
                 </label>
-            </div>
+									</div>
+									<div class="w-50 text-md-right">
+										<a href="#">نسيت كلمة المرور</a>
+									</div>
+		            </div>
+		          </form>
+		        </div>
+		      </div>
+				</div>
+			</div>
+		</div>
+	</section>
 
-            <div class="flex items-center justify-end mt-4">
-                @if (Route::has('password.request'))
-                    <a class="underline text-sm text-gray-600 hover:text-gray-900" href="{{ route('password.request') }}">
-                        {{ __('Forgot your password?') }}
-                    </a>
-                @endif
+	<script src="{{ asset('login-form-11\js\main.js') }}"></script>
+  <script src="{{ asset('login-form-11\js\popper.js') }}"></script>
+  <script src="{{ asset('login-form-11\js\bootstrap.min.js') }}"></script>
+  <script src="{{ asset('login-form-11\js\main.js') }}"></script>
 
-                <x-jet-button class="ml-4">
-                    {{ __('Log in') }}
-                </x-jet-button>
-            </div>
-        </form>
-    </x-jet-authentication-card>
+	</body>
+</html>
+
+
 </x-guest-layout>
